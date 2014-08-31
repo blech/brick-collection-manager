b593b63d2e0bb4130e92595193c6ad082e82bb3e
Paul Mison <misonp+git@gmail.com>
2014-08-31 13:46:14 -0700

    Tidy up admin

--- lcm/setlist/admin.py
+++ lcm/setlist/admin.py
@@ -4,7 +4,7 @@ from .models import LegoSet
 class LegoSetAdmin(admin.ModelAdmin):
     list_display = ('set_number', 'set_name', 'chain', 'vendor', 'online', 'used', 
                     'date_acquired', 'total_price')
-    list_filter = ('theme', 'subtheme', 'date_acquired')
+    list_filter = ('date_acquired', 'chain', 'theme')
     ordering = ('-date_acquired',)
 admin.site.register(LegoSet, LegoSetAdmin)
 
